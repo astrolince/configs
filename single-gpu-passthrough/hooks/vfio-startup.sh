dc73723a628b8759eed08aab886202058483fd21
astrolince <astro@astrolince.com>
2020-11-04 23:17:43 -0300

    adding vfio files

---
+++ single-gpu-passthrough/hooks/vfio-startup.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+long_delay=10
+medium_delay=5
+short_delay=1
+echo "Beginning of startup!"
+
+function stop_display_manager_if_running {
+    if systemctl is-active --quiet $1 ; then
+        echo $1 >> /tmp/vfio-store-display-manager
+        systemctl stop $1
+    fi
+
+    while systemctl is-active --quiet $1 ; do
+        sleep "${short_delay}"
+    done
+}
+
+function unload_module_if_loaded {
+    if lsmod | grep $1 &> /dev/null ; then
+    modprobe -r $1
+    echo $1 >> /tmp/vfio-loaded-gpu-modules
+    fi
+    while lsmod | grep $1 &> /dev/null ; do
+        sleep "${short_delay}"
+    done
+}
+
+function get_virsh_id {
+    python -c "print('pci_0000_'+'$1'.split(':')[0] + '_' + '$1'.split(':')[1].split('.')[0] + '_' + '$1'.split(':')[1].split('.')[1])"
+}
+
+function get_pci_id_from_device_id {
+    lspci -nn | grep $1 | awk '{print $1}'
+}
+
+# Stop currently running display manager
+if test -e "/tmp/vfio-store-display-manager" ; then
+    rm -f /tmp/vfio-store-display-manager
+fi
+stop_display_manager_if_running sddm.service
+stop_display_manager_if_running gdm.service
+stop_display_manager_if_running lightdm.service
+stop_display_manager_if_running lxdm.service
+stop_display_manager_if_running xdm.service
+stop_display_manager_if_running mdm.service
+stop_display_manager_if_running display-manager.service
+
+
+# Unbind VTconsoles if currently bound (adapted from https://www.kernel.org/doc/Documentation/fb/fbcon.txt)
+if test -e "/tmp/vfio-bound-consoles" ; then
+    rm -f /tmp/vfio-bound-consoles
+fi
+for (( i = 0; i < 16; i++))
+do
+  if test -x /sys/class/vtconsole/vtcon${i}; then
+      if [ `cat /sys/class/vtconsole/vtcon${i}/name | grep -c "frame buffer"` \
+           = 1 ]; then
+	       echo 0 > /sys/class/vtconsole/vtcon${i}/bind
+           echo "Unbinding console ${i}"
+           echo $i >> /tmp/vfio-bound-consoles
+      fi
+  fi
+done
+
+# According to kernel documentation (https://www.kernel.org/doc/Documentation/fb/fbcon.txt), 
+# specifically unbinding efi-framebuffer is not necessary after all consoles
+# are unbound (and often times harmful in my experience), so it was omitted here
+# I leave it here for reference in case anyone needs it.
+
+#Unbind EFI-Framebuffer if currently bound
+# if test -e "/sys/bus/platform/drivers/efi-framebuffer/unbind" ; then
+#     echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+# else
+#     echo "Could not find framebuffer to unload!"
+
+sleep "${long_delay}"
+
+# Unload loaded GPU drivers
+if test -e "/tmp/vfio-loaded-gpu-modules" ; then
+    rm -f /tmp/vfio-loaded-gpu-modules
+fi
+
+unload_module_if_loaded amdgpu-pro
+unload_module_if_loaded amdgpu
+unload_module_if_loaded nvidia_drm
+unload_module_if_loaded nvidia_modeset
+unload_module_if_loaded nvidia_uvm
+unload_module_if_loaded nvidia
+unload_module_if_loaded ipmi_devintf
+unload_module_if_loaded nouveau
+unload_module_if_loaded i915
+
+# Unbind the GPU from display driver
+if test -e "/tmp/vfio-virsh-ids" ; then
+    rm -f /tmp/vfio-virsh-ids
+fi
+if test -e "/tmp/vfio-pci-ids" ; then
+    rm -f /tmp/vfio-pci-ids
+fi
+
+gpu_device_id=$(modprobe -c vfio-pci | grep 'options vfio_pci ids' | cut -d '=' -f2 | cut -d ',' -f 1)
+#gpu_audio_device_id=$(modprobe -c vfio-pci | grep 'options vfio_pci ids' | cut -d '=' -f2 | cut -d ',' -f 2)
+gpu_pci_id=$(get_pci_id_from_device_id ${gpu_device_id})
+#gpu_audio_pci_id=$(get_pci_id_from_device_id ${gpu_audio_device_id})
+echo ${gpu_pci_id} >> /tmp/vfio-pci-ids
+#echo ${gpu_audio_pci_id} >> /tmp/vfio-pci-ids
+virsh_gpu_id=$(get_virsh_id ${gpu_pci_id})
+#virsh_gpu_audio_id=$(get_virsh_id ${gpu_audio_pci_id})
+echo ${virsh_gpu_id} >> /tmp/vfio-virsh-ids
+#echo ${virsh_gpu_audio_id} >> /tmp/vfio-virsh-ids
+
+virsh nodedev-detach "${virsh_gpu_id}"
+#virsh nodedev-detach "${virsh_gpu_audio_id}"
+
+# Load VFIO kernel module
+modprobe vfio-pci
+
+echo "End of startup!"
---
+++ single-gpu-passthrough/hooks/vfio-teardown.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -x
+
+echo "Beginning of teardown!"
+
+# Unload VFIO-PCI Kernel Driver
+modprobe -r vfio-pci
+modprobe -r vfio_iommu_type1
+modprobe -r vfio
+
+# Re-Bind GPU to AMD Driver
+input="/tmp/vfio-virsh-ids"
+while read virshId; do
+  virsh nodedev-reattach "$virshId"
+done < "$input"
+
+# Disconnect and reconnect GPU
+input="/tmp/vfio-pci-ids"
+while read pciId; do
+  echo 1 > /sys/bus/pci/devices/0000:${pciId}/remove
+done < "$input"
+echo 1 > /sys/bus/pci/rescan
+
+sleep 5
+
+# Rebind VT consoles (adapted from https://www.kernel.org/doc/Documentation/fb/fbcon.txt)
+input="/tmp/vfio-bound-consoles"
+while read consoleNumber; do
+  if test -x /sys/class/vtconsole/vtcon${consoleNumber}; then
+      if [ `cat /sys/class/vtconsole/vtcon${consoleNumber}/name | grep -c "frame buffer"` \
+           = 1 ]; then
+    echo "Rebinding console ${consoleNumber}"
+	  echo 1 > /sys/class/vtconsole/vtcon${consoleNumber}/bind
+      fi
+  fi
+done < "$input"
+
+# Hack that magically makes nvidia gpus work :)
+if command -v nvidia-xconfig ; then
+    nvidia-xconfig --query-gpu-info > /dev/null 2>&1
+fi
+
+# According to kernel documentation (https://www.kernel.org/doc/Documentation/fb/fbcon.txt), 
+# specifically unbinding efi-framebuffer is not necessary after all consoles
+# are unbound (and often times harmful in my experience), so it was omitted here
+# I leave it here for reference in case anyone needs it.
+
+# Re-Bind EFI-Framebuffer
+# if test -e "/sys/bus/platform/drivers/efi-framebuffer/bind" ; then
+#     echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
+# else
+#     echo "Could not find framebuffer to bind!"
+# fi
+
+#Load amd driver
+input="/tmp/vfio-loaded-gpu-modules"
+while read gpuModule; do
+  modprobe "$gpuModule"
+done < "$input"
+
+# Restart Display Manager
+input="/tmp/vfio-store-display-manager"
+while read displayManager; do
+  systemctl start "$displayManager"
+done < "$input"
+
+echo "End of teardown!"
