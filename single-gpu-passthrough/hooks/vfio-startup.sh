4b6b1f8d5dffcbb7d47478cedbb4f40c73a9de5f
astrolince <astro@astrolince.com>
2020-11-07 17:22:55 -0300

    libvirtd hooks comments

--- single-gpu-passthrough/hooks/vfio-startup.sh
+++ single-gpu-passthrough/hooks/vfio-startup.sh
@@ -3,18 +3,22 @@ set -x
 
 echo "Beginning of startup!"
 
+# Kill desktop manager
 systemctl stop sddm.service
 
 sleep 1
 
+# Switch to text mode
 systemctl isolate multi-user.target
 
 sleep 1
 
+# Load VFIO driver
 modprobe vfio_pci
 
 sleep 1
 
+# Reset GPU and bind it to VFIO driver
 echo 1 > /sys/bus/pci/devices/0000:09:00.0/remove
 echo "1002 731f" > /sys/bus/pci/drivers/amdgpu/remove_id
 echo "1002 731f" > /sys/bus/pci/drivers/vfio-pci/new_id
@@ -22,10 +26,12 @@ echo 1 > /sys/bus/pci/rescan
 
 sleep 1
 
+# Kill stuff that may still be running by your user
 killall -u astro
 
 sleep 1
 
+# Drop all RAM caches
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
--- single-gpu-passthrough/hooks/vfio-teardown.sh
+++ single-gpu-passthrough/hooks/vfio-teardown.sh
@@ -3,23 +3,27 @@ set -x
 
 echo "Beginning of teardown!"
 
-echo 1 | tee /sys/bus/pci/devices/0000:09:00.0/remove
-echo "1002 731f" | tee /sys/bus/pci/drivers/vfio-pci/remove_id
-echo "1002 731f" | tee /sys/bus/pci/drivers/amdgpu/new_id
-echo 1 | tee /sys/bus/pci/rescan
+# Reset GPU and bind it to AMD driver
+echo 1 > /sys/bus/pci/devices/0000:09:00.0/remove
+echo "1002 731f" > /sys/bus/pci/drivers/vfio-pci/remove_id
+echo "1002 731f" > /sys/bus/pci/drivers/amdgpu/new_id
+echo 1 > /sys/bus/pci/rescan
 
 sleep 2
 
+# Unload VFIO drivers
 modprobe -r vfio-pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
 
 sleep 2
 
+# Kill stuff that may still be running by your user
 killall -u astro
 
 sleep 6
 
+# Switch to graphical mode
 systemctl isolate graphical.target
 
 echo "End of teardown!"
